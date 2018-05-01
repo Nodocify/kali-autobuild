4c609bc250f2f086ddef3d0798b52f64861b1122
nodoc <blake@logicverse.net>
2018-04-30 21:49:21 -0600

    My tools...

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-#add all customizations here
+echo "Initialize MSFDB"
+msfdb init
+msfdb start
 
-exit
+echo "Install zsteg"
+gem install zsteg
+
+echo "Install Empire"
+git clone https://github.com/PowerShellEmpire/Empire.git
+cd Empire/setup
+STAGING_KEY=RANDOM ./install.sh
+echo "cd /root/Empire && ./empire" >> /usr/bin/empire
+chmod +x /usr/bin/empire
